924368d27ad955b7a53c4614949b0d7bdf4dab86
kaysmart001 <luwoyekayode99@gmail.com>
2023-01-30 07:01:35 +0100

    Update schema.sql

--- models/schema.sql
+++ models/schema.sql
@@ -25,76 +25,75 @@ SET time_zone = "+00:00";
 -- DROP TABLE Question;
 -- DROP TABLE Student_Result;
 
-
 -- Create the required tables
 
 CREATE TABLE 'Student' (
-    username VARCHAR(15) PRIMARY KEY NOT NULL,
-    firstName CHAR(100) NOT NULL,
-    lastName CHAR(100) NOT NULL,
-    imageUrl VARCHAR(255),
-    gender VARCHAR(50),
-    pw VARCHAR(255) NOT NULL -- password
+    'username' VARCHAR(15) PRIMARY KEY NOT NULL,
+    'firstName' CHAR(100) NOT NULL,
+    'lastName' CHAR(100) NOT NULL,
+    'imageUrl' VARCHAR(255),
+    'gender' VARCHAR(50),
+    'pw' VARCHAR(255) NOT NULL -- password
 );
 
 
 CREATE TABLE 'Administrator' (
-    username VARCHAR(255) PRIMARY KEY NOT NULL DEFAULT "admin",
-    firstName CHAR(100) NOT NULL DEFAULT "Admin",
-    lastName CHAR(100) NOT NULL DEFAULT "Admin",
-    imageUrl VARCHAR(255) DEFAULT "/images/admin.png",
-    pw VARCHAR(255) NOT NULL DEFAULT "$2y$10$B9gGv1ohRO.KubkLY1gyGuwmc0.SNdBYMME8cYsuvVDpC6YdBwNny" -- password
+    'username' VARCHAR(255) PRIMARY KEY NOT NULL DEFAULT "admin",
+    'firstName' CHAR(100) NOT NULL DEFAULT "Admin",
+    'lastName' CHAR(100) NOT NULL DEFAULT "Admin",
+    'imageUrl' VARCHAR(255) DEFAULT "/images/admin.png",
+    'pw' VARCHAR(255) NOT NULL DEFAULT "$2y$10$B9gGv1ohRO.KubkLY1gyGuwmc0.SNdBYMME8cYsuvVDpC6YdBwNny" -- password
 );
 
 CREATE TABLE 'Examiner' (
-    username VARCHAR(255) PRIMARY KEY NOT NULL,
-    firstName CHAR(100) NOT NULL,
-    lastName CHAR(100) NOT NULL,
-    imageUrl VARCHAR(255),
-    gender VARCHAR(50),
-    pw VARCHAR(255) NOT NULL -- password
+    'username' VARCHAR(255) PRIMARY KEY NOT NULL,
+    'firstName' CHAR(100) NOT NULL,
+    'lastName' CHAR(100) NOT NULL,
+    'imageUrl' VARCHAR(255),
+    'gender' VARCHAR(50),
+    'pw' VARCHAR(255) NOT NULL -- password
 
 );
 
 
 CREATE TABLE 'Course' (
-    courseId INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    courseTitle VARCHAR(255) NOT NULL,
-    courseCode VARCHAR(50) NOT NULL
+    'courseId' INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    'courseTitle' VARCHAR(255) NOT NULL,
+    'courseCode' VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE 'Exam' (
-    examId INT PRIMARY KEY AUTO_INCREMENT,
-    instruction VARCHAR(255),
-    timeDuration TIME NOT NULL,
-    activated BOOLEAN DEFAULT FALSE NOT NULL,
-    createdAt DATETIME NOT NULL,
-    lastModified DATETIME NOT NULL,
-    courseId INT,
+    'examId' INT PRIMARY KEY AUTO_INCREMENT,
+    'instruction' VARCHAR(255),
+    'timeDuration' TIME NOT NULL,
+    'activated' BOOLEAN DEFAULT FALSE NOT NULL,
+    'createdAt' DATETIME NOT NULL,
+    'lastModified' DATETIME NOT NULL,
+    'courseId' INT,
     FOREIGN KEY (courseId) REFERENCES Course(courseId)
 );
 
 
 CREATE TABLE 'Question' (
-    questionId INT PRIMARY KEY AUTO_INCREMENT,
-    question TEXT NOT NULL,
-    option1 VARCHAR(255) NOT NULL,
-    option2 VARCHAR(255) NOT NULL,
-    option3 VARCHAR(255) NOT NULL,
-    option4 VARCHAR(255) NOT NULL,
-    option5 VARCHAR(255), -- optional 
-    answer VARCHAR(255) NOT NULL,
-    examId INT,
+    'questionId' INT PRIMARY KEY AUTO_INCREMENT,
+    'question' TEXT NOT NULL,
+    'option1' VARCHAR(255) NOT NULL,
+    'option2' VARCHAR(255) NOT NULL,
+    'option3' VARCHAR(255) NOT NULL,
+    'option4' VARCHAR(255) NOT NULL,
+    'option5' VARCHAR(255), -- optional 
+    'answer' VARCHAR(255) NOT NULL,
+    'examId' INT,
     FOREIGN KEY (examId) REFERENCES Exam(examId)
 );
 
 
 CREATE TABLE 'Student_Result' (
-    studentId VARCHAR(255) NOT NULL,
-    examId INT NOT NULL,
-    score INT NOT NULL,
-    scoreOverall INT NOT NULL,
-    submitTime DATETIME NOT NULL,
+    'studentId' VARCHAR(255) NOT NULL,
+    'examId' INT NOT NULL,
+    'score' INT NOT NULL,
+    'scoreOverall' INT NOT NULL,
+    'submitTime' DATETIME NOT NULL,
     FOREIGN KEY (studentId) REFERENCES Student(username),
     FOREIGN KEY (examId) REFERENCES Exam(examId),
     PRIMARY KEY (studentId, examId)
@@ -104,18 +103,18 @@ CREATE TABLE 'Student_Result' (
 
 -- Intermediary table for Course and Student 
 CREATE TABLE 'Course_Student' (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(15) ,
-    courseId INT,
+    'id' INT PRIMARY KEY AUTO_INCREMENT,
+    'username' VARCHAR(15) ,
+    'courseId' INT,
     FOREIGN KEY (username) REFERENCES Student(username),
     FOREIGN KEY (courseId) REFERENCES Course(courseId)
 );
 
 -- Intermediary table for Course and Examiner 
 CREATE TABLE 'Course_Examiner' (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(255),
-    courseId INT,
+    'id' INT PRIMARY KEY AUTO_INCREMENT,
+    'username' VARCHAR(255),
+    'courseId' INT,
     FOREIGN KEY (username) REFERENCES Examiner(username),
     FOREIGN KEY (courseId) REFERENCES Course(courseId)
 );
@@ -136,7 +135,7 @@ SHOW TABLES;
 -- Load Student Data from CSV file
 LOAD DATA
 LOCAL INFILE "thekaysmart.epizy.com/test/models/students.csv"
-INTO TABLE Student
+INTO TABLE 'Student'
 FIELDS TERMINATED BY ","
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY "\n"
@@ -146,20 +145,20 @@ IGNORE 1 ROWS;
 -- Load Examiner Data from CSV file
 LOAD DATA
 LOCAL INFILE "thekaysmart.epizy.com/test/examiners.csv"
-INTO TABLE Examiner
+INTO TABLE 'Examiner'
 FIELDS TERMINATED BY ","
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY "\n"
 IGNORE 1 ROWS;
 -- SELECT * FROM Examiner;
 
-INSERT INTO Administrator VALUES ();
+INSERT INTO 'Administrator' VALUES ();
 -- SELECT * FROM Administrator;
 
 -- Load Course Data from CSV file
 LOAD DATA
 LOCAL INFILE "thekaysmart.epizy.com/test/models/courses.csv"
-INTO TABLE Course
+INTO TABLE 'Course'
 FIELDS TERMINATED BY ","
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY "\n"
@@ -170,7 +169,7 @@ IGNORE 1 ROWS
 -- Load Exam Data from CSV file
 LOAD DATA
 LOCAL INFILE "thekaysmart.epizy.com/test/models/examination.csv"
-INTO TABLE Exam
+INTO TABLE 'Exam'
 FIELDS TERMINATED BY ","
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY "\n"
@@ -182,7 +181,7 @@ IGNORE 1 ROWS
 -- Load Question Data from CSV file
 LOAD DATA
 LOCAL INFILE "thekaysmart.epizy.com/test/models/questions.csv"
-INTO TABLE Question
+INTO TABLE 'Question'
 FIELDS TERMINATED BY ","
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY "\n"
@@ -192,7 +191,7 @@ IGNORE 1 ROWS
 
 LOAD DATA
 LOCAL INFILE "thekaysmart.epizy.com/test/models/course-examiner.csv"
-INTO TABLE Course_Examiner
+INTO TABLE 'Course_Examiner'
 FIELDS TERMINATED BY ","
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY "\n"
@@ -202,7 +201,7 @@ IGNORE 1 ROWS
 
 LOAD DATA
 LOCAL INFILE "thekaysmart.epizy.com/test/models/course-student.csv"
-INTO TABLE Course_Student
+INTO TABLE 'Course_Student'
 FIELDS TERMINATED BY ","
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY "\n"
